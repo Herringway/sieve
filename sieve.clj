86f478ef98a5f116c3ed358acd404eeb601e8a8f
Cameron Ross <elpenguino@gmail.com>
2014-04-16 16:00:51 -0300

    threading...?

--- sieve.clj
+++ sieve.clj
@@ -1,14 +1,19 @@
 (use 'clojure.set)
 (defn multiples-to-n
 	[multiplier limit]
-	(remove #(>= multiplier %) (take-nth multiplier (range (+ limit 1))))
+	(->> (range (+ limit 1))
+		(take-nth multiplier)
+		(remove #(>= multiplier %)))
 ) ; multiples-to-n
 (defn get-multiple-lists
 	[limit]
-	(map #(multiples-to-n % limit) (remove #(> 2 %) (range (/ limit 2))))
+	(->> (remove #(> 2 %) (range (/ limit 2)))
+		(map #(multiples-to-n % limit)))
 ) ; get-multiple-lists
 (defn sieve
 	[limit]
-	(sort (apply difference (set (remove #(> 2 %) (range limit))) (get-multiple-lists limit)))
+	(as-> (get-multiple-lists limit) $
+		(apply difference (set (remove #(> 2 %) (range limit))) $)
+		(sort $))
 ) ; sieve
-(println (sieve 1000))
+(println (sieve 10000))
